b08832b85d0da5306899165366238afcc28ad4ea
deekshth <manasvaram58@gmail.com>
2020-05-25 11:42:47 -0400

    first commit

---
+++ eclipse-workspace/project/src/test/java/Test/project/SampleXmlIT.java
@@ -0,0 +1,16 @@
+package Test.project;
+
+
+/**
+ * This is a sample Citrus integration test for loading XML syntax test case.
+ *
+ * @author Citrus
+ */
+
+public class SampleXmlIT {
+	
+	public static void main(String args[]) {
+		System.out.println("output came");
+	}
+}
+    
